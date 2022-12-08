f3a0a384d1e2cc5f9d4d6a9344c78533db57d704
Nilesh <nilesh@haachico.com>
2022-12-08 17:46:39 +0530

    add css and functionality

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const inputBox = document.querySelector(".input-box");
 const submitButton = document.querySelector(".submit-btn");
-const output = document.querySelector(".output");
+const outputLetterCount = document.querySelector(".output1");
+const outputWordCount = document.querySelector(".output2");
+const outputSentenceCount = document.querySelector(".output3");
+const outputParaCount = document.querySelector(".output4");
 
 function letterCounter(str) {
   const lettersCount = str.split("").filter((c) => c != " ").length;
@@ -8,14 +11,46 @@ function letterCounter(str) {
 }
 
 function wordCounter(str) {
-  const wordsCount = str.split("").filter((word) => word !== "").length;
+  const wordsCount = str.split(" ").length;
   return wordsCount;
+}
+// console.log(wordCounter(" hello how do you do"));
+
+function sentenceCounter(str) {
+  const sentencesCount = str.split(/[.|!|?]+/g).length - 1;
+  return sentencesCount;
+}
+// console.log(sentenceCounter("hello. how are you? i am nilesh."));
+
+function paraCounter(str) {
+  const parasCount = str.replace(/\n$/gm, "").split(/\n/).length;
+  return parasCount;
 }
 
 function checkCount() {
+  // console.clear();
   const inputText = inputBox.value;
-  const letterCount = letterCounter(inputText);
-  output.innerText = `letters: ${letterCount}`;
+
+  if (inputText === "") {
+    outputLetterCount.innerText = `Letters count: 0`;
+    outputWordCount.innerText = `Words count: 0`;
+    outputSentenceCount.innerText = `Sentences count: 0`;
+    outputParaCount.innerText = `Paragraphs count: 0`;
+    console.log("1st cond");
+  } else {
+    console.log("2nd cond");
+    const letterCount = letterCounter(inputText);
+    outputLetterCount.innerText = `Letters count: ${letterCount}`;
+
+    const wordCount = wordCounter(inputText);
+    outputWordCount.innerText = `Words count: ${wordCount}`;
+
+    const sentenceCount = sentenceCounter(inputText);
+    outputSentenceCount.innerText = `Sentences count: ${sentenceCount}`;
+
+    const paraCount = paraCounter(inputText);
+    outputParaCount.innerText = `Paragraphs count: ${paraCount}`;
+  }
 }
 
 submitButton.addEventListener("click", checkCount);
